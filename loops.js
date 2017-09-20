4d360327b1cfd7b1de09a8d983553e3085c8b857
Man Leung <lepusleung@gmail.com>
2017-09-20 18:09:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,29 +1,34 @@
-function forLoop(array) {
-  for (var i = 0 ; i <= 25 ; i ++) {
-    if (i === 1) {
-      array.push("I am 1 strange loop.")
-    } else {
-      array.push("I am ${i} strange loops.")
+function forLoop(arr){
+
+    for (var i=0 ;i<25;i++){
+      if (i===1){
+          arr.push("I am 1 strange loop.")
+      }
+      else{
+        arr.push(`I am ${i} strange loops.`)
+      }
     }
-  }
-  return array;
+    return arr
 }
 
-function whileLoop(num) {
-  while (countdown > 0) {
-    console.log(--countdown)
+function whileLoop(num){
+
+  while (num>0){
+    console.log(--num)
   }
-  return 'done';
+  return "done"
 }
+
 
 function doWhileLoop(array){
 
-  function maybeTrue() {
+
+function maybeTrue() {
   return Math.random() >= 0.5
 }
 
-  do {
-    array.pop();
-  }
-  while (array.length > 0 && maybeTrue());
-  return array;
+
+  do{ array=array.pop } while (array.length > 0 && maybeTrue());
+
+
+  return array
